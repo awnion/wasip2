8b9bda479361b6b23566e07811c117f4eb723d38
Sergei Blinov <blinovsv@gmail.com>
2025-04-25 18:42:33 +0200

    fmt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from wasmtime import Store
+
 # The magic, refer to https://github.com/bytecodealliance/wasmtime-py?tab=readme-ov-file#usage
-import wasmtime.loader
+import wasmtime.loader  # noqa: F401
+
 from ww import Root
 
 
